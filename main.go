333dcd85222fb8b5da9980ac0ed6415ebcc2d2b7
ZeroxC0DE <ravenspar@protonmail.com>
2025-02-22 20:50:03 +0530

    some fixes

--- main.go
+++ main.go
@@ -17,4 +17,17 @@ func main() {
 		os.Exit(1)
 	}
 
+	u := tea.NewProgram(cmd.InitialUploadModel())
+	if _, err := u.Run(); err != nil {
+		fmt.Printf("Error: %v", err)
+		os.Exit(1)
+	}
+
+	manageModel := cmd.InitialManageModel()
+	q := tea.NewProgram(manageModel)
+	if _, err := q.Run(); err != nil {
+		fmt.Printf("Error: %v", err)
+		os.Exit(1)
+	}
+
 }
